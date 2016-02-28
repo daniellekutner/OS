308da420231b397aa19a5b9b0a958b3b847b7ac8
daniellekutner <daniellekutner@gmail.com>
2016-02-28 11:16:29 +0200

    fix main

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 #include "osm.h"
 int main()
 {
+    osm_init();
     osm_operation_time(1000);
     osm_function_time(1000);
     osm_syscall_time(1000);
     osm_disk_time(1000);
+    osm_finalizer();
     return 0;
 }
